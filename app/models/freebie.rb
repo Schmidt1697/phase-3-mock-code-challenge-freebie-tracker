680d59108e7e2094bab3eb689493768af814b6ed
Anna Schmidt <marshall1697@gmail.com>
2022-10-18 09:36:09 -0600

    freebie methods

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,13 @@
 class Freebie < ActiveRecord::Base
+    belongs_to :company
+    belongs_to :dev
+
+    def freebie_dev
+        self.dev
+    end
+
+    def freebie_company
+        self.company
+    end
 
 end
